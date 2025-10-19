9824ae3964e626ed4396137be5970c1aa5de040c
reader001-guius <3157539545@qq.com>
2025-10-19 20:53:02 +0800

    the change of documents

--- cheak_system.py
+++
@@ -1,5 +0,0 @@
-import torch, numpy, matplotlib
-print("torch:", torch.__version__)
-print("numpy:", numpy.__version__)
-print("matplotlib:", matplotlib.__version__)
-print("CUDA 可用?:", torch.cuda.is_available())
